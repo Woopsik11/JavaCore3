f1d6a12da8845e458de81f64d26a45f12c19b076
Woops <vantage59@icloud.com>
2022-02-19 15:06:57 +0500

    Initial commit

---
+++ src/main/java/HomeWork1/Apple.java
@@ -0,0 +1,7 @@
+package HomeWork1;
+
+public class Apple extends Fruit{
+    public Apple() {
+        weight=1.0f;
+    }
+}
---
+++ src/main/java/HomeWork1/Orange.java
@@ -0,0 +1,7 @@
+package HomeWork1;
+
+public class Orange extends Fruit {
+    public Orange() {
+        weight=1.5f;
+    }
+}
